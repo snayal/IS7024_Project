cf028c04f37a3b0ce2cf6bb4f555c16950fc9110
Shashi <47906013+snayal@users.noreply.github.com>
2021-03-26 22:12:07 -0400

    updated index.cshtml.cs
    removed crime data

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 using QuickType;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json.Schema;
-using QuickTypeCrimes;
+
 
 namespace IS7024_Project.Pages
 {
@@ -24,25 +24,7 @@ namespace IS7024_Project.Pages
 
         public void OnGet()
         {
-            using (var webClient = new WebClient())
-            {
-                string crimesJSON = webClient.DownloadString("https://data.cincinnati-oh.gov/resource/k59e-2pvf.json");
-                JSchema crimeSchema = JSchema.Parse(System.IO.File.ReadAllText("CrimeSchema.json"));
-                JArray crimeArray = JArray.Parse(crimesJSON);
-                IList<string> validationEventsCrime = new List<string>();
-                if(crimeArray.IsValid(crimeSchema, out validationEventsCrime))
-                {
-                    var crime = Crimes.FromJson(crimesJSON);
-                    ViewData["Crime"] = crime;
-                }
-                else
-                {
-                    foreach(string evt in validationEventsCrime)
-                    {
-                        Console.WriteLine(evt);
-                    }
-                    ViewData["Crime"] = new List<Crimes>();
-                }
+            
 
                 string jsonString = webClient.DownloadString("https://data.cincinnati-oh.gov/resource/m76i-p5p9.json");
                 JSchema schema = JSchema.Parse(System.IO.File.ReadAllText("PropertySchema.json"));
