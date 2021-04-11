54511245ebb0b1a3dd7c21702d04b8ca44c80eeb
Damon <63312122+nichold5@users.noreply.github.com>
2021-04-11 11:42:19 -0400

    Revert "Updated majority of site design, Search box now scans all columns"
    This reverts commit 8bf3ed0e9ff5c1ae3150e9667224907c031d643a.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,10 +2,3 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
-
-var input = document.querySelectorAll('input');
-for (i = 0; i < input.length; i++) {
-    input[i].setAttribute('size', input[i].getAttribute('placeholder').length);
-}
-
-
