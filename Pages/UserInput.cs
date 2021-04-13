0d1cafdabb809ab49544142e47ce6ef3041e40f0
ioanaguna <gunaia@mail.uc.edu>
2021-04-12 22:16:53 -0400

    * CovidVaccineUserInput.cshtml: * CovidVaccineUserInput.cshtml.cs: created new page * UserInput.cs: new class * UserInputRoster.cs: new class * CovidVaccine.cshtml: linked to the page

---
+++ Pages/UserInput.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IS7024_Project.Pages
+{
+    public class UserInput
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Location { get; set; }
+    }
+}
---
+++ Pages/UserInputRoster.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace IS7024_Project.Pages
+{
+    public static class UserInputRoster
+    {
+        static UserInputRoster()
+        {
+            allUserInputs = new List<UserInput>();
+        }
+
+        public static IList<UserInput> allUserInputs { get; set; }
+    }
+}
